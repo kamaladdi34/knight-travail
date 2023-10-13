95906cb0664bde9f9b03353fdc85970939646023
Addi Kamal <kamal.addi@proton.me>
2023-10-13 01:19:00 +0100

    Commit this non working solution

--- knight-travail.js
+++ knight-travail.js
@@ -34,3 +34,60 @@ function printBoard() {
     console.log(row);
   }
 }
+function positionsAreSame(position, targetPosition) {
+  return position.x == targetPosition.x && position.y == targetPosition.y;
+}
+function findClosestPath(position, target) {
+  let possibleMoves = possibleMovesFromPosition(position);
+  let moves = [];
+  for (let i = 0; i < possibleMoves.length; i++) {
+    let result = findPath(possibleMoves[i], target, position);
+    if (result != undefined) {
+      moves.push(result);
+    }
+  }
+  console.log(moves);
+}
+function findPath(
+  position,
+  target,
+  lastSecond,
+  depth = 1,
+  moves = [],
+  step = 1
+) {
+  console.log(position, lastSecond, step);
+  if (step == 2) {
+    step = 0;
+    if (positionsAreSame(position, lastSecond)) {
+      console.log("position already visited");
+      return;
+    }
+    lastSecond = position;
+  }
+  moves.push(position);
+  if (positionsAreSame(position, target)) {
+    return moves;
+  } else if (depth > 10) {
+    return undefined;
+  }
+  let possibleMoves = possibleMovesFromPosition(position);
+  for (let i = 0; i < possibleMoves.length; i++) {
+    let d = depth;
+    let s = step;
+    let result = findPath(
+      possibleMoves[i],
+      target,
+      lastSecond,
+      ++d,
+      [...moves],
+      ++s
+    );
+    if (result != undefined) {
+      return result;
+    }
+  }
+}
+let start = { x: 0, y: 0 };
+let target = { x: 4, y: 4 };
+console.log(findClosestPath(start, target));
